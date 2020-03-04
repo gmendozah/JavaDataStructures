c74cad700b3259168f69e5d1748e890ae0d43620
hypersaiph <geovanimendoza.h@gmail.com>
2020-03-03 20:57:15 -0400

    Queue class

---
+++ src/Queue.java
@@ -0,0 +1,42 @@
+public class Queue {
+    public class Node {
+        private Node next;
+        private int data;
+
+        private Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int peak() {
+        return this.head.data;
+    }
+
+    public void add(int data) {
+        Node node = new Node(data);
+        if (tail != null) {
+            tail.next = node;
+        }
+        tail = node;
+        if (head == null) {
+            head = node;
+        }
+    }
+
+    public int remove() {
+        int data = head.data;
+        head = head.next;
+        if(head == null) {
+            tail = null;
+        }
+        return data;
+    }
+
+}
