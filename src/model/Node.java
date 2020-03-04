e477a070a7ad0584d5188e85b2b58d558dcc5b72
gmendozah <geovanimendoza.h@gmail.com>
2020-03-04 07:42:54 -0400

    Creating node class

---
+++ src/model/Node.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Node {
+    Node next;
+    int data;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public void append(int data) {
+        Node current = this;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = new Node(data);
+    }
+}
